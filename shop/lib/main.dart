20f6c384ec7b6755c3f11559d8ce0daf22515ae8
Kirti Gautam <gautamkirti8c@gmail.com>
2022-01-20 20:45:12 +0530

    Shop App started

---
+++ shop/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shop'),
+      ),
+      body: const Center(
+        child: Text('Home Page'),
+      ),
+    );
+  }
+}
